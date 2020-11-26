17db7d2221a5e09db957537fa4db92b5f688066a
Tom Lo <loto2@oocl.com>
2020-11-26 14:00:21 +0800

    [fizz-buzz][Tom] test: implement test for case for first special number

--- src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
@@ -17,4 +17,17 @@ public class FizzBuzzTest {
         // then
         assertEquals("1", actual);
     }
+
+    @Test
+    void should_return_Fizz_when_count_off_given_order_num_of_multiple_of_first_special_num() {
+        // given
+        int orderNum = 3;
+        FizzBuzz fizzBuzz = new FizzBuzz();
+
+        // when
+        String actual = fizzBuzz.countOff(orderNum);
+
+        // then
+        assertEquals("Fizz", actual);
+    }
 }
