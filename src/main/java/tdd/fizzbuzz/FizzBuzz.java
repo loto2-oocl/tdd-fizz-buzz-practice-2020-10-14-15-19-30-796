3f19d4d594d3730b003fd0bd9a26692ad29ecba0
Tom Lo <loto2@oocl.com>
2020-11-26 14:11:55 +0800

    [fizz-buzz][Tom] refactor: extract output for special number as constant

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -1,17 +1,21 @@
 package tdd.fizzbuzz;
 
 public class FizzBuzz {
+    public static final String FIZZ = "Fizz";
+    public static final String BUZZ = "Buzz";
+    public static final String WHIZZ = "Whizz";
+
     public String countOff(int orderNum) {
         if (orderNum % 3 == 0) {
-            return "Fizz";
+            return FIZZ;
         }
 
         if (orderNum % 5 == 0) {
-            return "Buzz";
+            return BUZZ;
         }
 
         if (orderNum % 7 == 0) {
-            return "Whizz";
+            return WHIZZ;
         }
 
         return String.valueOf(orderNum);
