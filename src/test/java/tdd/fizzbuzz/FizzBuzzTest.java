88c4c6b34865ad5a44cf9676e71c1f0c3f6ca5f4
Tom Lo <loto2@oocl.com>
2020-11-26 13:57:04 +0800

    [fizz-buzz][Tom] test: implement test for standard case

--- src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
@@ -1,4 +1,20 @@
 package tdd.fizzbuzz;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class FizzBuzzTest {
+    @Test
+    void should_return_String_order_num_when_count_off_given_order_num() {
+        // given
+        int orderNum = 1;
+        FizzBuzz fizzBuzz = new FizzBuzz();
+
+        // when
+        String actual = fizzBuzz.countOff(orderNum);
+
+        // then
+        assertEquals("1", actual);
+    }
 }
