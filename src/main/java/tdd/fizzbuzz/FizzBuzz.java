b677120d2330a8b80f31d1121ea235f91cc6d4dd
Tom Lo <loto2@oocl.com>
2020-11-26 14:12:42 +0800

    [fizz-buzz][Tom] refactor: extract value of special number as constant

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -4,17 +4,20 @@ public class FizzBuzz {
     public static final String FIZZ = "Fizz";
     public static final String BUZZ = "Buzz";
     public static final String WHIZZ = "Whizz";
+    public static final int FIRST_SPECIAL_NUM = 3;
+    public static final int SECOND_SPECIAL_NUM = 5;
+    public static final int THIRD_SPECIAL_NUM = 7;
 
     public String countOff(int orderNum) {
-        if (orderNum % 3 == 0) {
+        if (orderNum % FIRST_SPECIAL_NUM == 0) {
             return FIZZ;
         }
 
-        if (orderNum % 5 == 0) {
+        if (orderNum % SECOND_SPECIAL_NUM == 0) {
             return BUZZ;
         }
 
-        if (orderNum % 7 == 0) {
+        if (orderNum % THIRD_SPECIAL_NUM == 0) {
             return WHIZZ;
         }
 
