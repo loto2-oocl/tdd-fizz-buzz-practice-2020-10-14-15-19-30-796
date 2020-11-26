71222d052b18fca636d14a2e2e238e4f77f5f05c
Tom Lo <loto2@oocl.com>
2020-11-26 14:01:45 +0800

    [fizz-buzz][Tom] feat: implementation for case of first special number

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -2,6 +2,10 @@ package tdd.fizzbuzz;
 
 public class FizzBuzz {
     public String countOff(int orderNum) {
+        if (orderNum % 3 == 0) {
+            return "Fizz";
+        }
+
         return String.valueOf(orderNum);
     }
 }
